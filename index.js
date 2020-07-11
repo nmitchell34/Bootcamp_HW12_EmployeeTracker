3e1558ec158e82e3ee68ac24e29b2fa080a59a77
NCM <nmitchell34@gatech.edu>
2020-07-10 22:33:00 -0400

    got second 2 working

--- index.js
+++ index.js
@@ -52,10 +52,10 @@ function init() {
           );
           break;
         case "View All Employees by Department":
-            byDeptOrRole("department","departments","Department")
+          byDeptOrRole("department", "departments", "Department");
           break;
         case "View All Employees by Role":
-            byDeptOrRole("title","roles","Role")
+          byDeptOrRole("title", "roles", "Role");
           break;
         case "Add Employee":
           break;
@@ -96,24 +96,38 @@ function viewEmployees() {}
 // Initiation Function
 init();
 
-
-function byDeptOrRole(col,table,deptOrRole){
-    console.log("SELECT " + col + " FROM " + table)
-    connection.query("SELECT ?? FROM ??",[col,table],function(err,data){
-        if (err) throw err;
-        console.log(data)
-        console.log(data[1])
-        choicesArr=[]
-        for (i=0;i<data.length;i++){
-            choicesArr.push(data[i].col)
-        }
-        inquirer.prompt({
-            name:"deptRoleSelection",
-            message: "Which "+deptOrRole+" would you like to see?",
-            type: "list",
-            choices: choicesArr
-        }).then((answers)=>{
-            // answers.deptRoleSelection
-        })
-    })
+function byDeptOrRole(col, table, deptOrRole) {
+  console.log("SELECT " + col + " FROM " + table);
+  connection.query("SELECT ?? FROM ??", [col, table], function (err, data) {
+    if (err) throw err;
+    choicesArr = [];
+    if (col === "department") {
+      for (i = 0; i < data.length; i++) {
+        choicesArr.push(data[i].department);
+      }
+    } else {
+      for (i = 0; i < data.length; i++) {
+        choicesArr.push(data[i].title);
+      }
+    }
+    inquirer
+      .prompt({
+        name: "deptRoleSelection",
+        message: "Which " + deptOrRole + " would you like to see?",
+        type: "list",
+        choices: choicesArr,
+      })
+      .then((answers) => {
+        connection.query(
+          "SELECT employees.employee_id, employees.first_name, employees.last_name, departments.department, roles.title, roles.salary FROM employees INNER JOIN departments ON employees.department_id=departments.department_id INNER JOIN roles ON employees.role_id=roles.role_id WHERE ??.?? = ?",
+          [table, col, answers.deptRoleSelection],
+          function (err, data) {
+            if (err) throw err;
+            console.table(data);
+            init();
+          }
+        );
+        // answers.deptRoleSelection
+      });
+  });
 }
